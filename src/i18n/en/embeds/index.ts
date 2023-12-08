5e8a7c61e34e93c5525e0ef7637b0a6897c716c1
Dieouwe <135071604+Die0uwe@users.noreply.github.com>
2023-12-08 20:00:28 +0100

    nederlands-index.ts
    nederlands

--- src/i18n/en/embeds/index.ts
+++ src/i18n/en/embeds/index.ts
@@ -1,76 +1,76 @@
 import type { BaseTranslation } from "../../i18n-types";
 
-const en_embeds = {
-	help: {
-		title: "{username}'s commands",
-		description: "Here is a list of all my commands. You can also see them when you type `/` in Discord.",
-	},
-	about: {
-		description:
-			'{username} is an intuitive and user-friendly Discord bot connected to https://paliapedia.com and has been created by {developers}.',
-		statistics_name: 'Statistics',
-		statistics_value: 'Servers: {servers}\nUsers: {users}',
-		debug_name: "Debug",
-		debug_value: "Shards: {shards}\nShardID: {shardId}",
-	},
-	item: {
-		category: 'Category',
-		rarity: 'Rarity',
-		quality: 'Quality',
-		cost: 'Cost',
-		value: 'Value',
-		sold_at: 'Sold at',
-		crafted_by: 'Crafted by',
-		gathered_from: 'Gathered from',
-		quest_reward_from: 'Quest reward from',
-		attached_to: 'Attached to',
-		obtained_by_reading: 'Obtained by reading',
-		reward_from: 'Reward from',
-		obtained_from: 'Obtained from',
-		teaches_recipe: 'Teaches recipe',
-		needed_for_quest: 'Needed for quest',
-		contains_item: 'Contains item',
-	},
-	quest: {
-		starts_with: 'Starts with',
-		requirement: 'Requirement',
-		requirements: 'Requirements',
-		branching_goals: 'Complete {count} of the following',
-		turn_in: 'Turn in',
-		quest_return: 'Quest return',
-		reward: 'Reward',
-		rewards: 'Rewards',
-		friendship_points: 'x{amount} Friendship Points with {villager}',
-		romance_points: 'x{amount} Romance Points with {villager}',
-		mail_message: "{villager}'s mail",
-		visit_from: 'A visit from {villager}',
-	},
-	recipe: {
-		cost: 'Cost',
-		craft_time: 'Craft time',
-		required_ingredients: 'Required ingredients',
-		produces: 'Produces',
-		sold_at: 'Sold at',
-		vendors: 'Vendors',
-		gatherables: 'Gatherables',
-		mail: 'Mail',
-		recipes: 'Recipes',
-	},
-	villager: {
-		gift_preferences: 'Gift preferences',
-		weekly_wants: 'Weekly wants',
-		relationship:
-			'{type} level {level} - {levelName} (Requires {requiredValue})',
-	},
-	weekly_wants: {
-		title: 'Weekly Wants Rotation',
-		description:
-			'The weekly wants rotation for {date} is now available! You can find the full list of weekly wants on {url}.',
-	},
-	miscellaneous: {
-		see_more: 'See {count} more ...',
-		no_data: 'No data available.',
-	},
-} satisfies BaseTranslation;
+const nl_embeds = {
+  help: {
+    title: "{username}'s commando's",
+    description: "Hier is een lijst met al mijn commando's. Je kunt ze ook zien wanneer je `/` typt in Discord.",
+  },
+  about: {
+    description:
+      '{username} is een intuïtieve en gebruiksvriendelijke Discord-bot verbonden met https://paliapedia.com en is gemaakt door {developers}.',
+    statistics_name: 'Statistieken',
+    statistics_value: 'Servers: {servers}\nGebruikers: {users}',
+    debug_name: "Debug",
+    debug_value: "Shards: {shards}\nShardID: {shardId}",
+  },
+  item: {
+    category: 'Categorie',
+    rarity: 'Zeldzaamheid',
+    kwaliteit: 'Kwaliteit',
+    cost: 'Kosten',
+    value: 'Waarde',
+    verkocht_bij: 'Verkocht bij',
+    gemaakt_door: 'Gemaakt door',
+    verzameld_uit: 'Verzameld uit',
+    quest_beloning_van: 'Quest beloning van',
+    gehecht_aan: 'Gehecht aan',
+    verkregen_door_te_lezen: 'Verkregen door te lezen',
+    beloning_van: 'Beloning van',
+    verkregen_uit: 'Verkregen uit',
+    leert_recept: 'Leert recept',
+    nodig_voor_quest: 'Nodig voor quest',
+    bevat_item: 'Bevat item',
+  },
+  quest: {
+    begint_met: 'Begint met',
+    vereiste: 'Vereiste',
+    vereisten: 'Vereisten',
+    tak_doelen: 'Voltooi {count} van de volgende',
+    inleveren_bij: 'Inleveren bij',
+    quest_terugkeer: 'Quest terugkeer',
+    beloning: 'Beloning',
+    beloningen: 'Beloningen',
+    vriendschap_punten: 'x{amount} Vriendschapspunten met {villager}',
+    romantiek_punten: 'x{amount} Romantiekpunten met {villager}',
+    mail_bericht: "{villager}'s post",
+    bezoek_van: 'Een bezoek van {villager}',
+  },
+  recipe: {
+    kosten: 'Kosten',
+    craft_tijd: 'Craft tijd',
+    benodigde_ingredienten: 'Benodigde ingrediënten',
+    produceert: 'Produceert',
+    verkocht_bij: 'Verkocht bij',
+    verkopers: 'Verkopers',
+    verzamelbare_items: 'Verzamelbare items',
+    post: 'Post',
+    recepten: 'Recepten',
+  },
+  villager: {
+    cadeau_voorkeuren: 'Cadeau voorkeuren',
+    wekelijkse_wensen: 'Wekelijkse wensen',
+    relatie:
+      '{type} niveau {level} - {levelName} (Vereist {requiredValue})',
+  },
+  wekelijkse_wensen: {
+    title: 'Rotatie van Wekelijkse Wensen',
+    description:
+      'De rotatie van wekelijkse wensen voor {date} is nu beschikbaar! Je kunt de volledige lijst van wekelijkse wensen vinden op {url}.',
+  },
+  diverse: {
+    zie_meer: 'Zie {count} meer ...',
+    geen_gegevens: 'Geen gegevens beschikbaar.',
+  },
+} voldoet aan BaseTranslation;
 
-export default en_embeds;
+export default nl_embeds;
