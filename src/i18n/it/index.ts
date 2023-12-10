b9ee3bc6e2cc058f432da6cb6b974f69b380b9c0
Dieouwe <135071604+Die0uwe@users.noreply.github.com>
2023-12-10 13:01:55 +0100

    Update index.ts
    base edit

--- src/i18n/it/index.ts
+++ src/i18n/it/index.ts
@@ -1,7 +1,7 @@
 import type { BaseTranslation } from '../i18n-types';
 
-const en = {
-  locale: 'en',
+const it = {
+  locale: 'it',
 } satisfies BaseTranslation;
 
-export default en;
+export default it;
